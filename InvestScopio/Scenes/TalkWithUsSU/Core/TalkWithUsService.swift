148f040d801a5dff0e1658755216d526ccebaf99
João Gabriel de Medeiros Pereira <gah.mp1@gmail.com>
2020-09-29 17:52:08 -0300

    removendo codigos nao usados mais, finalizando tela de fale conosco

---
+++ InvestScopio/Scenes/TalkWithUsSU/Core/TalkWithUsService.swift
@@ -0,0 +1,9 @@
+//
+//  TalkWithUsService.swift
+//  InvestScopio
+//
+//  Created by Joao Gabriel Pereira on 29/09/20.
+//  Copyright © 2020 Joao Medeiros Pereira. All rights reserved.
+//
+
+import Foundation
---
+++ InvestScopio/SharedFeatures/SwiftUI/MailView/MailView.swift
@@ -0,0 +1,21 @@
+//
+//  MailView.swift
+//  InvestScopio
+//
+//  Created by Joao Gabriel Pereira on 29/09/20.
+//  Copyright © 2020 Joao Medeiros Pereira. All rights reserved.
+//
+
+import SwiftUI
+
+struct MailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MailView()
+    }
+}
